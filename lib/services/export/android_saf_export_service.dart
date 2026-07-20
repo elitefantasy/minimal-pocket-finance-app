b0fe94cd4e4f28b515300d1ca5b8661a86ce6394
elitefantasy <wearexakm@gmail.com>
2026-07-20 10:17:41 +0530

    feat: add full export path resolution for Android database and CSV exports

--- lib/services/export/android_saf_export_service.dart
+++ lib/services/export/android_saf_export_service.dart
@@ -72,11 +72,68 @@ class AndroidSafExportService {
       overwrite: false,
     );
 
+    final absolutePath = _resolveAbsolutePath(
+      rootUri: rootUri,
+      artifactFolder: artifactFolder,
+      fileName: fileName,
+    );
+
     // Return exported filename.
     return ExportResult(
       fileName: fileName,
       relativePath:
           '${AppConstants.appName}/$artifactFolder/$fileName',
+      absolutePath: absolutePath,
     );
+  }
+
+  String? _resolveAbsolutePath({
+    required String rootUri,
+    required String artifactFolder,
+    required String fileName,
+  }) {
+    try {
+      final decodedUri = Uri.decodeComponent(rootUri);
+
+      String treePath = decodedUri;
+      if (treePath.contains('/tree/')) {
+        treePath = treePath.split('/tree/').last;
+      } else if (treePath.contains('/document/')) {
+        treePath = treePath.split('/document/').last;
+      }
+
+      if (treePath.contains('?')) {
+        treePath = treePath.split('?').first;
+      }
+
+      String basePath = '';
+      if (treePath.startsWith('primary:')) {
+        final subPath = treePath.substring('primary:'.length);
+        basePath = '/storage/emulated/0${subPath.isEmpty ? '' : '/$subPath'}';
+      } else if (treePath.startsWith('raw:')) {
+        basePath = treePath.substring('raw:'.length);
+      } else if (RegExp(r'^[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}:').hasMatch(treePath)) {
+        final parts = treePath.split(':');
+        final volume = parts.first;
+        final subPath = parts.sublist(1).join(':');
+        basePath = '/storage/$volume${subPath.isEmpty ? '' : '/$subPath'}';
+      } else {
+        final colonIndex = treePath.indexOf(':');
+        if (colonIndex != -1 && colonIndex < treePath.length - 1) {
+          basePath = '/storage/emulated/0/${treePath.substring(colonIndex + 1)}';
+        } else {
+          basePath = treePath;
+        }
+      }
+
+      final subDirectory = '${AppConstants.appName}/$artifactFolder/$fileName';
+      if (basePath.endsWith('/')) {
+        return '$basePath$subDirectory';
+      } else {
+        return '$basePath/$subDirectory';
+      }
+    } catch (_) {
+      return null;
+    }
   }
 }
